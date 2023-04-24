6f2dcdf37315905fa04d2e2592ccf3609c507a90
HugoCanovas <canovashugo83@gmail.com>
2023-04-24 20:39:14 +0200

    affichage des derniers films

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&l
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        console.log(data.results);
+        let result = data.results;
+        result.map(item => {
+            console.log(item.title);
+            const newDiv = document.createElement('div');
+            const newContent = document.createTextNode(item.title);
+            const newImg = document.createElement('img');
+            newImg.src = "https://image.tmdb.org/t/p/w500"+item.backdrop_path;
+            document.body.append(newDiv);
+            document.body.append(newImg);
+            newDiv.appendChild(newContent);
+        });
     })
     // .catch(error => {
     //     console.error(error);
